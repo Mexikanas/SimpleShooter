6612a197beeac7525a6f781b66af140bd311d96f
Mexikanas <mexikanas@gmail.com>
2021-08-30 10:20:48 +0900

    Added controller support

--- Source/SimpleShooter/Characters/ShooterCharacter.h
+++ Source/SimpleShooter/Characters/ShooterCharacter.h
@@ -28,5 +28,10 @@ protected:
 private: 
 	void MoveForward(float AxisValue);
 	void MoveRight(float AxisValue);
+	void LookUpRate(float AxisValue);
+	void LookRightRate(float AxisValue);
+
+	UPROPERTY(EditAnywhere)
+		float RotationRate = 10.f;
 
 };
